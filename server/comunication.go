0baf16737dfe298604b745648c1bdd34958269ae
agustinaa235 <asegura@fi.uba.ar>
2022-11-04 15:23:50 -0300

    Feat: add comunication struct

---
+++ server/comunication.go
@@ -0,0 +1,51 @@
+package server
+
+import (
+	"fmt"
+	"strconv"
+	"truco/app/common"
+)
+
+func sendMenu(player Player) (string, error) {
+	common.Send(player.socket, "Bienvenido al truco "+player.name)
+	common.Receive(player.socket)
+	common.Send(player.socket, "Las reglas del juego son sencillas: .....")
+	common.Receive(player.socket)
+	common.Send(player.socket, "ingresa CREATE para crear un juego O ingresa JOIN para uniser a una partida ya creada")
+	// receives its answer
+	messagePlayer, error := common.Receive(player.socket)
+	return messagePlayer, error
+}
+
+func processRequest(player Player, message string) map[string]int {
+	match := make(map[string]int)
+	fmt.Println("Mesnaje: ", message)
+	if message == "CREATE" {
+		fmt.Println("Entre a create")
+		common.Send(player.socket, "Creando una partia, ingresar de cuantos integrantes 2 o 4")
+		members, _ := common.Receive(player.socket)
+		common.Send(player.socket, "Ingrese duracion de partida partida: 15 o 30 puntos")
+		duration, _ := common.Receive(player.socket)
+		fmt.Println("duration ", duration)
+		fmt.Println("members ", members)
+		match["create"] = 0
+		match["members"], _ = strconv.Atoi(members)
+		match["duration"], _ = strconv.Atoi(duration)
+		common.Send(player.socket, "Partida creada, esperando a que se unan el resto de los jugadores")
+		return match
+	} else {
+		fmt.Println("Entre a join")
+		common.Send(player.socket, "Buscando una partida, de cuantos integrastes; 2 o 4")
+		members, _ := common.Receive(player.socket)
+		common.Send(player.socket, "Ingrese duracion de partida partida: 15 o 30 puntos")
+		duration, _ := common.Receive(player.socket)
+		fmt.Println("duration ", duration)
+		fmt.Println("members ", members)
+		match["create"] = 1
+		match["members"], _ = strconv.Atoi(members)
+		match["duration"], _ = strconv.Atoi(duration)
+		common.Send(player.socket, "Partida solicitada, se esta buscando una partida")
+		return match
+	}
+
+}
