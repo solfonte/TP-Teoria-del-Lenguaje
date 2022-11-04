83331042b8ef5f2b7d7693d1dedf061e5b0928a0
agustinaa235 <asegura@fi.uba.ar>
2022-11-04 11:15:03 -0300

    Feat: add client run flow

--- client/client.go
+++ client/client.go
@@ -3,8 +3,11 @@ package main
 //  The net module lets you make network connections and transmit data.
 
 import (
+	"bufio"
 	"fmt"
 	"net"
+	"os"
+	"truco/app/common"
 )
 
 const (
@@ -16,21 +19,56 @@ const (
 func main() {
 
 	// connect to server
-	conn, err := net.Dial(SERVER_TYPE, SERVER_HOST+":"+SERVER_PORT)
+	socket, err := net.Dial(SERVER_TYPE, SERVER_HOST+":"+SERVER_PORT)
 	if err != nil {
+		fmt.Println("error")
 		panic(err)
 	}
 
-	// sending some data to server
-	_, err = conn.Write([]byte("Este es un mensaje de prueba"))
-	// buffer where the data that the server sendas will be store
-	buffer := make([]byte, 1024)
-	// read what the server send
-	dataLen, error := conn.Read(buffer)
-	if error != nil {
-		fmt.Println("Error reading:", error.Error())
+	// // sending some data to server
+	// _, err = conn.Write([]byte("Este es un mensaje de prueba"))
+	// // buffer where the data that the server sendas will be store
+	// buffer := make([]byte, 1024)
+	// // read what the server send
+	// dataLen, error := conn.Read(buffer)
+	// if error != nil {
+	// 	fmt.Println("Error reading:", error.Error())
+	// }
+	// fmt.Println("Received: ", string(buffer[:dataLen]))
+	// // close connection
+	fmt.Println("entre a client run")
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		messageServer, error := common.Receive(socket)
+		if error != nil {
+			fmt.Println("Error reciving from server: ", error.Error())
+			socket.Close()
+		}
+		fmt.Println("Message server: ", messageServer)
+		messageClient, _ := reader.ReadString('\n')
+		if messageClient == "close" {
+			fmt.Println("Stop playing")
+			return
+		}
+		common.Send(socket, messageClient)
 	}
-	fmt.Println("Received: ", string(buffer[:dataLen]))
-	// close connection
-	defer conn.Close()
 }
+
+// func runClient(socket net.Conn) {
+// 	fmt.Println("entre a client run")
+// 	reader := bufio.NewReader(os.Stdin)
+// 	for {
+// 		messageServer, error := common.Receive(socket)
+// 		if error != nil {
+// 			fmt.Println("Error reciving from server: ", error.Error())
+// 			socket.Close()
+// 		}
+// 		fmt.Println("Message server: ", messageServer)
+// 		messageClient, _ := reader.ReadString('\n')
+// 		if messageClient == "close" {
+// 			fmt.Println("Stop playing")
+// 			return
+// 		}
+// 		common.Send(socket, messageClient)
+// 	}
+// }
