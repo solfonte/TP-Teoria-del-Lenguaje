f6514416ae3ec08bbcacea4ecf4830e4e7c439e4
agustinaa235 <asegura@fi.uba.ar>
2022-11-04 15:24:40 -0300

    Feat: add create match logic

--- server/matchManager.go
+++ server/matchManager.go
@@ -1,11 +1,25 @@
 package server
 
+import "fmt"
+
 type MatchManager struct {
+	matches []Match
+}
+
+func process_player(matchManager MatchManager, player Player) {
+	askPlayerName(player)
+	messageClient, _ := sendMenu(player)
+	requestedmatch := processRequest(player, messageClient)
+	if requestedmatch["create"] == 0 {
+		newMatch := Match{duration: requestedmatch["duration"], maxPlayers: requestedmatch["members"]}
+		newMatch.players = append(newMatch.players, player)
+		matchManager.matches = append(matchManager.matches, newMatch)
+		fmt.Println("Matches ", matchManager.matches)
+	} else {
+		go look_matches_with_criteria(requestedmatch["duration"], requestedmatch["members"])
+	}
+}
+
+func look_matches_with_criteria(duration int, cantPlayers int) {
 	
 }
-
-func add_player(matchManager MatchManager, player Player) {
-	askPlayerName(player)
-}
-
-
