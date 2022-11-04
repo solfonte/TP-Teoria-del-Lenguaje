9d372db757f9764ab985603ce7273f0a1574e2d9
agustinaa235 <asegura@fi.uba.ar>
2022-11-04 18:49:59 -0300

    Feat: add players to match

--- server/match.go
+++ server/match.go
@@ -1,7 +1,18 @@
 package server
 
+import "fmt"
+
 type Match struct {
 	duration   int
 	maxPlayers int
 	players    []Player
+	started    bool
+}
+
+func addPlayerToMatch(match *Match, player Player) {
+	match.players = append(match.players, player)
+	if len(match.players) == match.maxPlayers {
+		match.started = true
+		fmt.Println("Comenzo partida")
+	}
 }
