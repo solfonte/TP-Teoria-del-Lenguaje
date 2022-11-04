b3fe4e652a8f4f201e5879a64cba2df0dafdaf63
agustinaa235 <asegura@fi.uba.ar>
2022-11-04 11:15:44 -0300

    Refactor: porcess clinet in aceptor

--- server/acceptor.go
+++ server/acceptor.go
@@ -8,35 +8,41 @@ import (
 
 type Acceptor struct {
 	listenerSocket net.Listener
+	players        []Player
 }
 
 func start_receiver(acceptor Acceptor) {
+	matchManager := MatchManager{}
 	fmt.Printf(("estoy en receiver"))
 	for {
 		// acept diferent connections
-		conn, error := acceptor.listenerSocket.Accept()
+		peer, error := acceptor.listenerSocket.Accept()
 		if error != nil {
 			fmt.Println("Error accepting: ", error.Error())
 			os.Exit(1)
 		}
+		newPlayer := Player{id: 1, name: "pepe", socket: peer}
+		acceptor.players = append(acceptor.players, newPlayer)
+		add_player(matchManager, newPlayer)
+
 		fmt.Println("client connected")
-		go process_client(conn)
+		//go process_client(peer)
 	}
 
 }
 
-func process_client(connection net.Conn) {
-	buffer := make([]byte, 1024)
-	// leo lo que me llega
-	dataLen, error := connection.Read(buffer)
-	if error != nil {
-		fmt.Println("Error reading:", error.Error())
-		connection.Close()
-	}
-	fmt.Println("Received: ", string(buffer[:dataLen]))
-	_, error = connection.Write([]byte("Llego este mensaje al server: " + string(buffer[:dataLen])))
-	connection.Close()
-}
+// func process_client(connection net.Conn) {
+// 	buffer := make([]byte, 1024)
+// 	// leo lo que me llega
+// 	dataLen, error := connection.Read(buffer)
+// 	if error != nil {
+// 		fmt.Println("Error reading:", error.Error())
+// 		connection.Close()
+// 	}
+// 	fmt.Println("Received: ", string(buffer[:dataLen]))
+// 	_, error = connection.Write([]byte("Llego este mensaje al server: " + string(buffer[:dataLen])))
+// 	connection.Close()
+// }
 
 func stop_receiver(acceptor Acceptor) {
 	acceptor.listenerSocket.Close()
