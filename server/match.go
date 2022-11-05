e5c74f37afc41d0f7ceab0b2c38fceb47e46a0c3
solfonte <msfontenla@fi.uba.ar>
2022-11-05 11:19:13 -0300

    moved card dealing logic  to carddealer

--- server/match.go
+++ server/match.go
@@ -1,11 +1,11 @@
 package server
-
+/*
 import (
-	//"fmt"
+	"fmt"
 	"math/rand"
     "time"
 )
-
+*/
 type Match struct {
 	duration   int
 	maxPlayers int
@@ -15,32 +15,12 @@ type Match struct {
 
 func deal_cards(players []Player){
 	
-	//inicializamos un numero por carta
-	var cards [40]int
-	for i := range cards {
-		cards[i] = 1
-	}
+	var cardDealer  = CardDealer{}
+	cardDealer.initialize()
 
-	//random generator para asignar al azar
-	rand.Seed(time.Now().UnixNano())
-
-	//se le asignan cartas a cada jugador. No se pueden repetir. 
 	for _,p := range players {
 
-		var amountOfCards int = 0
-		var assignedCards [3]int
-
-		for amountOfCards < 3 {
-			card := rand.Int() % 40
-			if cards[card] != 0 {
-				assignedCards[amountOfCards] = card
-
-				//se le asigna cero para determinar que ya se repartio
-				cards[card] = 0
-				amountOfCards += 1
-			}
-		} 
-		p.dealCards(assignedCards)
+		cardDealer.assignCards(&p)
 	}
 }
 
