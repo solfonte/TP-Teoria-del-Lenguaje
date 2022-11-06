821a62bbae40d6ce6bbe053655ad5f9704224f9f
Stephanieizquierdo <sizquierdo@fi.uba.ar>
2022-11-06 00:46:58 -0300

    Refactor de currentPlayer

--- server/round.go
+++ server/round.go
@@ -11,7 +11,7 @@ type Round struct {
 	players       []Player
 	moves         int
 	hand          int
-	currentPlayer int
+	currentPlayer *Player
 	championId    int
 	envido        bool
 }
@@ -20,7 +20,7 @@ func (Round *Round) initialize(players []Player) {
 	Round.players = players
 	Round.moves = 0
 	Round.hand = rand.Int() % len(players)
-	Round.currentPlayer = Round.hand
+	Round.currentPlayer = &players[Round.hand]
 	Round.championId = -1
 	Round.envido = false
 }
@@ -36,46 +36,46 @@ func (Round *Round) canSingEnvido() bool {
 }
 
 func (Round *Round) handleEnvido() {
-	common.Send(Round.players[Round.currentPlayer].socket, "cantaste ENVIDO")
+	common.Send(Round.currentPlayer.socket, "cantaste ENVIDO")
 	fmt.Println("cantaste ENVIDO")
 }
 
 func (Round *Round) handleThrowACard() int {
-	common.Send(Round.players[Round.currentPlayer].socket, "Que carta queres tirar?")
+	common.Send(Round.currentPlayer.socket, "Que carta queres tirar?")
 	fmt.Println("dasdas")
-	common.Send(Round.players[Round.currentPlayer].socket, "1) "+Round.players[Round.currentPlayer].cards[0].getFullName())
+	common.Send(Round.currentPlayer.socket, "1) "+Round.currentPlayer.cards[0].getFullName())
 	fmt.Println("dasdas")
-	common.Send(Round.players[Round.currentPlayer].socket, "2) "+Round.players[Round.currentPlayer].cards[1].getFullName())
+	common.Send(Round.currentPlayer.socket, "2) "+Round.currentPlayer.cards[1].getFullName())
 	fmt.Println("dasdas")
-	common.Send(Round.players[Round.currentPlayer].socket, "3) "+Round.players[Round.currentPlayer].cards[2].getFullName())
+	common.Send(Round.currentPlayer.socket, "3) "+Round.currentPlayer.cards[2].getFullName())
 	fmt.Println("dasdas")
 
-	common.Send(Round.players[Round.currentPlayer].socket, "Seleccione: ")
+	common.Send(Round.currentPlayer.socket, "Seleccione: ")
 
-	jugada, _ := common.Receive(Round.players[Round.currentPlayer].socket)
+	jugada, _ := common.Receive(Round.currentPlayer.socket)
 	option, _ := strconv.Atoi(jugada)
 	return option
 }
 
 func (Round *Round) askPlayerForMove() {
 	fmt.Println("EN LA ROUND")
-	fmt.Println(Round.players[Round.currentPlayer])
-	for i := 0; i <= len(Round.players) && i != Round.currentPlayer; i++ {
+	fmt.Println(Round.currentPlayer)
+	for i := 0; i <= len(Round.players) && Round.players[i].id != Round.currentPlayer.id; i++ {
 		common.Send(Round.players[i].socket, "Espera a que juegue tu oponente...")
 	}
-	common.Send(Round.players[Round.currentPlayer].socket, "Podes hacer las siguientes jugadas:")
-	common.Send(Round.players[Round.currentPlayer].socket, "1) tirar una carta")
+	common.Send(Round.currentPlayer.socket, "Podes hacer las siguientes jugadas:")
+	common.Send(Round.currentPlayer.socket, "1) tirar una carta")
 	fmt.Println("SADSADSA") //Investigar como hacer para hacer multiples sends sin que se trabe
 	if Round.canSingEnvido() {
-		common.Send(Round.players[Round.currentPlayer].socket, "2) cantar envido")
+		common.Send(Round.currentPlayer.socket, "2) cantar envido")
 		fmt.Println("SADSADSA") //Investigar como hacer para hacer multiples sends sin que se trabe
 	}
 
-	common.Send(Round.players[Round.currentPlayer].socket, "3) cantar truco")
+	common.Send(Round.currentPlayer.socket, "3) cantar truco")
 	fmt.Println("SADSADSA") //Investigar como hacer para hacer multiples sends sin que se trabe
-	common.Send(Round.players[Round.currentPlayer].socket, "Seleccione: ")
+	common.Send(Round.currentPlayer.socket, "Seleccione: ")
 
-	jugada, _ := common.Receive(Round.players[Round.currentPlayer].socket)
+	jugada, _ := common.Receive(Round.currentPlayer.socket)
 	option, _ := strconv.Atoi(jugada)
 
 	switch option {
@@ -84,7 +84,7 @@ func (Round *Round) askPlayerForMove() {
 	case 2:
 		Round.handleEnvido()
 	case 3:
-		common.Send(Round.players[Round.currentPlayer].socket, "cantaste TRUCO")
+		common.Send(Round.currentPlayer.socket, "cantaste TRUCO")
 		fmt.Println("cantaste TRUCO")
 	}
 }
